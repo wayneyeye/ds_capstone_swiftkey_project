203c7045d85040e3b599f9ceb1a10e9b6b70fa86
yewenhe0904 <yewenhe0904@gmail.com>
2017-06-10 18:22:06 -0500

    training set

--- testing/01-sampling.py
+++ testing/01-sampling.py
@@ -1,4 +1,4 @@
-import sys,os,logging,re,pprint
+import sys,os,logging,re,pprint,random
 #Logging header
 LOGGING_LVL={
 	"debug":logging.DEBUG,
@@ -23,23 +23,37 @@ for file in os.listdir(dataFilePath):
 	"size":os.path.getsize(os.path.join(dataFilePath,file))
 	}
 logging.debug(dataFileDict)
+
 # open desired text file
 file2read=input("which file to read?\n"+" ".join(dataFileDict.keys())+"\n")
 fileObj=open(dataFileDict[file2read]["path"])
+
 # create sample text file path
-sampleFilePath=os.path.join(os.getcwd(),"sample-data",file2read+"-sample.txt")
+sampleFilePath_training=os.path.join(os.getcwd(),"training-data",file2read+"-training.txt")
+sampleFilePath_testing=os.path.join(os.getcwd(),"testing-data",,file2read+"-testing.txt")
+
 # purge existing file,if any
-if os.path.exists(sampleFilePath):
-	print(sampleFilePath+" already exists, will be removed before moving on")
-	os.remove(sampleFilePath)
+if os.path.exists(sampleFilePath_testing):
+	print(sampleFilePath_testing+" already exists, will be removed before moving on")
+	os.remove(sampleFilePath_testing)
+if os.path.exists(sampleFilePath_training):
+	print(sampleFilePath_training+" already exists, will be removed before moving on")
+	os.remove(sampleFilePath_training)
+
 # create sample file
-saveObj=open(sampleFilePath,"a")
-i=1
+trainingSet=open(sampleFilePath_training,"a")
+testingSet=open(sampleFilePath_testing,"a")
+
+# 8 of 10 go to training 2 go to testing
+sampleCutoff=80
+
 for line in fileObj:
-	saveObj.write(line)
-	i+=1
-	if i>=30:
-		break
+	if (random.randint(1,100)<=sampleCutoff):
+		trainingSet.write(line)
+	else:
+		testingSet.write(line)
+
 #close file objects after usage
 fileObj.close()
-saveObj.close()
+trainingSet.close()
+testingSet.close()
