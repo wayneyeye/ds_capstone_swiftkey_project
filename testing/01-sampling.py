dbec1f04e8537117fdc9e3c5d6737d568284b1d3
yewenhe0904 <yewenhe0904@gmail.com>
2017-06-10 18:42:26 -0500

    add sampling script

--- testing/01-sampling.py
+++ testing/01-sampling.py
@@ -1,4 +1,5 @@
 import sys,os,logging,re,pprint,random
+import courseraLib
 #Logging header
 LOGGING_LVL={
 	"debug":logging.DEBUG,
@@ -25,35 +26,46 @@ for file in os.listdir(dataFilePath):
 logging.debug(dataFileDict)
 
 # open desired text file
-file2read=input("which file to read?\n"+" ".join(dataFileDict.keys())+"\n")
-fileObj=open(dataFileDict[file2read]["path"])
+file2read=input("which file to read?\n type all to generate all\n"+" ".join(dataFileDict.keys())+"\n")
+fileList=[]
+if file2read.lower()=="all":
+	fileList=list(dataFileDict.keys())
+else:
+	fileList.append(file2read)
 
-# create sample text file path
-sampleFilePath_training=os.path.join(os.getcwd(),"training-data",file2read+"-training.txt")
-sampleFilePath_testing=os.path.join(os.getcwd(),"testing-data",,file2read+"-testing.txt")
+for f in fileList:
+	fileObj=open(dataFileDict[f]["path"])
+	# create sample text file path
+	sampleFilePath_training=os.path.join(os.getcwd(),"training-data",f+"-training.txt")
+	sampleFilePath_testing=os.path.join(os.getcwd(),"testing-data",f+"-testing.txt")
 
-# purge existing file,if any
-if os.path.exists(sampleFilePath_testing):
-	print(sampleFilePath_testing+" already exists, will be removed before moving on")
-	os.remove(sampleFilePath_testing)
-if os.path.exists(sampleFilePath_training):
-	print(sampleFilePath_training+" already exists, will be removed before moving on")
-	os.remove(sampleFilePath_training)
+	# purge existing file,if any
+	if os.path.exists(sampleFilePath_testing):
+		print(sampleFilePath_testing+" already exists, will be removed before moving on")
+		os.remove(sampleFilePath_testing)
+	if os.path.exists(sampleFilePath_training):
+		print(sampleFilePath_training+" already exists, will be removed before moving on")
+		os.remove(sampleFilePath_training)
 
-# create sample file
-trainingSet=open(sampleFilePath_training,"a")
-testingSet=open(sampleFilePath_testing,"a")
+	# create sample file
+	trainingSet=open(sampleFilePath_training,"a")
+	testingSet=open(sampleFilePath_testing,"a")
 
-# 8 of 10 go to training 2 go to testing
-sampleCutoff=80
+	# 8 of 10 go to training 2 go to testing
+	sampleCutoff=80
+	# Initial call to print 0% progress
+	i = 0
+	l = len(list(open(dataFileDict[f]["path"])))
+	courseraLib.printProgressBar(i, l, prefix = 'Sampling '+f+":", suffix = 'Complete')
+	for line in fileObj:
+		if (random.randint(1,100)<=sampleCutoff):
+			trainingSet.write(line)
+		else:
+			testingSet.write(line)
+		i+=1
+		courseraLib.printProgressBar(i, l, prefix = 'Sampling '+f+":", suffix = 'Complete')
 
-for line in fileObj:
-	if (random.randint(1,100)<=sampleCutoff):
-		trainingSet.write(line)
-	else:
-		testingSet.write(line)
-
-#close file objects after usage
-fileObj.close()
-trainingSet.close()
-testingSet.close()
+	#close file objects after usage
+	fileObj.close()
+	trainingSet.close()
+	testingSet.close()
